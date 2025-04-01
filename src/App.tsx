00d9dc4a0a5c5efc6864bd0160e69c1901ec47c6
carlosra10 <carloseduardoramirez10@gmail.com>
2025-03-31 22:45:44 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <>
+      <h1>
+        Hola Vite + React + TypeScript + Tailwind CSS + Vitest + React Testing Library!
+      </h1>
+    </>
+  )
+}
+
+export default App
