956c469969d38f04f1c95ff4e0a4cc0dfae69ef1
Carlosra10 <carloseduardoramirez10@gmail.com>
2025-04-04 11:51:50 -0400

    Colocando enel State la ciudad y país a consultar clima

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,33 +1,63 @@
+import { ChangeEvent, useState } from "react";
+import { SearchType } from "../../types";
 import { countries } from "../../data/countries";
 import styles from "./Form.module.css";
 
 export default function Form() {
-  return (
-    <form className={styles.form}>
-        <div className={styles.field}>
-            <label htmlFor="city">Ciudad:</label>
-            <input 
-                id="city"
-                type="text"
-                name="city"
-                placeholder="Ciudad"
-            />
-        </div>
+    const [search, setSearch] = useState<SearchType>({
+        city: "",
+        country: ""
+    });
 
-        <div className={styles.field}>
-            <label htmlFor="pais">País:</label>
-            <select>
-                <option id="opc" value="">--- Seleccione un País ---</option>
-                {countries.map(country=>(
-                    <option
-                    key={country.code}
-                    value={country.name}
-                    >{country.name}</option>
-                ))}
-            </select>
-        </div>
+    //el elemento lo lees desde el name del input o select
+    //el value lo lees desde el value del input o select
+    //handleChange es una función que se ejecuta cuando el usuario cambia el valor de un input o select
+    const handleChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
+        setSearch({
+            //hacemos una copia del estate
+            ...search,//...search copia las propiedades actuales del estado ({city: "", country: ""})
+            //actualizamos el valor del input o select que cambió
+            //e.target.name = "city" o "country" (porque el input y select tiene name="city" name="country")
+            //[e.target.name] usa el nombre del input como clave para actualizar el estado
+            [e.target.name]: e.target.value//e.target.value asigna el nuevo valor :Ejemplo - cada letra que vaya ingresando ("M") 
+        });
+    }
 
-        <input className={styles.submit} type="submit" value='Consultar Clima'/>
-    </form>
-  )
-}
+    return (
+        <form className={styles.form}>
+            <div className={styles.field}>
+                <label htmlFor="city">Ciudad:</label>
+{/*El input tiene un atributo name="city" que coincide con la propiedad del estado - El value está vinculado a search.city-  El evento onChange llama a handleChange cuando el valor cambia */}  
+                <input 
+                    id="city"
+                    type="text"
+                    name="city"
+                    placeholder="Ciudad"
+                    value={search.city}//El input ahora muestra "lo que escribimos" porque su value está vinculado al value de search.city
+                    onChange={handleChange}//llama a handleChange cuando el valor cambia , guarda el valor en el estado y luego lo muestra en el input
+                />
+            </div>
+            
+
+            <div className={styles.field}>
+                <label htmlFor="country">País:</label>
+                <select
+                    id="country"
+                    value={search.country}
+                    name="country"
+                    onChange={handleChange}
+                >
+                    <option value="">--- Seleccione un País ---</option>
+                    {countries.map(country=>(
+                        <option
+                        key={country.code}
+                        value={country.name}
+                        >{country.name}</option>
+                    ))}
+                </select>
+            </div>
+
+            <input className={styles.submit} type="submit" value='Consultar Clima'/>
+        </form>
+    )
+    }
--- src/data/countries.ts
+++ src/data/countries.ts
@@ -1,4 +1,8 @@
-export const countries = [
+import type { Country } from '../types'
+
+//countries es un array de objetos que contiene el código y el nombre de los países
+// el código es un string de 2 letras y el nombre es un string
+export const countries: Country[] = [
     { code: 'US', name: 'Estados Unidos' },
     { code: 'MX', name: 'México' },
     { code: 'AR', name: 'Argentina' },
