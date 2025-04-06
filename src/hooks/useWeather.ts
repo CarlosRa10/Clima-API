dda0ac9cab9ee036b1332ace5863a0212a59e3aa
carlosra10 <carloseduardoramirez10@gmail.com>
2025-04-05 23:23:34 -0400

    Refactor useWeather hook to streamline weather fetching functionality

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,12 +1,9 @@
-
-
 export default function useWeather() {
     
     //función que va a consultar el clima -  fetch(buscar el clima)
     const fetchWeather =  () => {
         console.log('Consultando...')
     }
-
     return{
         fetchWeather
     }
